1fce8786f75c016bfd66449df464f96a53dee06e
Isimbilysa <lysaisimbi7@gmail.com>
2024-04-10 18:24:35 +0200

    some commits

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+export default function App(){
+  return(
+ <div>
+  <Navbar/>
+  <Hero/>
+  <Analytics/>
+  <Newsletter/>
+  <Cards/>
+  <Footer/>
+ </div>
+  )
+}
